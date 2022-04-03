8cd1b4f4aa96840a4a5644c522c4a4128567c560
Jocelyn Strmec <strmecjocelyn@gmail.com>
2022-04-03 11:50:33 -0700

    train slpit

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 # Building a Convolutional Neural Network to classifiy computer 
 # attacks as types of malware
+from sklearn.model_selection import train_test_split
 import data_processing as d
 X, y = d.get_encoded_data()
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.33, random_state=1)
