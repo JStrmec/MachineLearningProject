96308da970ff92ffce5d86d3d2251c8c041acc1c
Jocelyn Strmec <strmecjocelyn@gmail.com>
2022-04-17 12:25:59 -0700

    got cnn in main running

--- data_processing.py
+++ data_processing.py
@@ -3,6 +3,7 @@ from sklearn.model_selection import train_test_split
 from keras.preprocessing.text import Tokenizer                    
 from keras.preprocessing.sequence import pad_sequences
 from sklearn.preprocessing import OneHotEncoder,LabelEncoder,StandardScaler,LabelBinarizer
+from sklearn.feature_extraction.text import CountVectorizer
 import pandas as pd
 import numpy as np
 
@@ -12,59 +13,35 @@ def read_csv():
     return df
 
 
-def get_data(return_x_y = False): 
+def get_data(): 
     # Read in csv file
     df = read_csv()
-    if(return_x_y):
-        return df["api"].values, df['class'].values
-    else:
-        df.drop['file']
-        return df
-
+    return df["api"].values, df['class'].values
 
 def get_encoded_data():
     # Variables
-    set_api = set() 
-    list_of_classes =['Adware', 'Agent', 'Backdoor', 'Trojan', 'Virus', 'Worms', 'Downloader', 'Spyware', 'Ransomware', 'Riskware', 'Dropper', 'Crypt','Keylogger']
-    x_data = []  
-    y_data = []
-    full_api_values_list =[]
-    x_temp = []
+    y_data = np.zeros((9795, 13)) # (9795,13)
+    list_of_classes =['Adware', 'Agent', 'Backdoor', 'Trojan', 'Virus', 'Worms', 'Downloader', 'Spyware', 'Ransomware', 'Riskware', 'Dropper', 'Crypt', 'Keylogger']
+    x,y = get_data()
 
-    # Read in csv file
-    df = read_csv()
+    # tokenizing
+    tokenizer = Tokenizer()
+    tokenizer.fit_on_texts(x)
+    x_train = tokenizer.texts_to_sequences(x) 
+    # Pad sequences with zeros
+    x_train = pad_sequences(x_train, padding='post', maxlen=100)
 
-    # Extract string column api to split for encoding
-    api_col = df["api"]
-    class_col = df['class']
-
-    # Loop to split rows of api column 
-    for x in api_col:
-        temp = x.split(',')
-        temp.append('stop')
-        x_temp.append(temp)
-        full_api_values_list.extend(temp)
-        set_api.update(temp)
-
-    # Create matrix of 0 to begin encoding with
-    x_data = np.zeros((api_col.size, len(set_api))) # (9795,7966)
-    y_data = np.zeros((api_col.size, 13)) # (9795,13)
-
-    # Encode x data 
-    # Convert set to list to get indexs for next step
-    list_api = list(set_api)
-
-    index = 0
-    for x in full_api_values_list:
-        if(x=='stop'):
-            index+=1
-        if(index == 9795):
-            break
-        x_data[index][list_api.index(x)] = 1
     count = 0
-    for x in class_col:
-        y_data[count][list_of_classes.index(x)] = 1
+    for z in y:
+        y_data[count][list_of_classes.index(z)] = 1
         count+=1
-        
-    return x_data, y_data
+
+    return x_train,y_data
+
+
+def encodeData(X , y):
+  y = LabelEncoder().fit_transform(y)
+  onehot_encoded = OneHotEncoder(handle_unknown="ignore",sparse=False).fit_transform(X)
+  print(y.shape())
+  return onehot_encoded, y
 
--- main.py
+++ main.py
@@ -2,38 +2,139 @@
 # attacks as types of malware
 import torch.nn as nn
 import torch
+import numpy as np
 import data_processing as d
-from sklearn.model_selection import train_test_split
+from torch.autograd import Variable
 from keras.preprocessing.text import Tokenizer
 from keras.preprocessing.sequence import pad_sequences
+from sklearn.metrics import roc_auc_score, log_loss, roc_auc_score, roc_curve, auc
 
-X, y = d.get_encoded_data()
-print(X.shape, y.shape)
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.33, random_state=1)
+X, y= d.get_encoded_data()
 
-#####MESSING AROUND#############
-# Importing libraries
-from keras.models import Sequential
-from keras.layers import Dense,Flatten
-from keras import layers
-from keras.layers.convolutional import Conv1D,MaxPooling1D
-from keras.layers.embeddings import Embedding
-from keras.preprocessing import sequence
+def XnumpyToTensor(x_data_np):
+    X_tensor = Variable(torch.from_numpy(x_data_np).type(torch.FloatTensor)) # Note the conversion for pytorch
+    
+    print(type(X_tensor.data)) # should be 'torch.cuda.FloatTensor'            
+    print((X_tensor.data.shape)) # torch.Size([108405, 29])
+    return X_tensor
 
-# Building the CNN Model
-model = Sequential()
-model.add(layers.Conv1D(128, 5, activation='relu',  input_shape=[1,7966], padding='same'))
-model.add(layers.GlobalMaxPooling1D())
-model.add(layers.Flatten())
-model.add(layers.Dense(26, activation='relu'))
-model.add(layers.Dense(13, activation='sigmoid'))
-model.compile(optimizer='adam',
-              loss='mse',
-              metrics=['accuracy'])
+def YnumpyToTensor(y_data_np):    
+    Y_tensor = Variable(torch.from_numpy(y_data_np)).type(torch.FloatTensor)  # BCEloss requires Float        
 
-# Fitting the data onto model
-model.fit(X_train, y_train, validation_data=(X_test, y_test), epochs=2, batch_size=128, verbose=2)
-# Getting score metrics from our model
-scores = model.evaluate(X_test, y_test, verbose=0)
-# Displays the accuracy of correct sentiment prediction over test data
-print("Accuracy: %.2f%%" % (scores[1]*100))
+    print(type(Y_tensor.data)) # should be 'torch.cuda.FloatTensor'
+    print(y_data_np.shape)
+    print(type(y_data_np))    
+    return Y_tensor
+
+
+# Dimensions
+# Number of features for the input layer
+N_FEATURES=X.shape[1]
+# Number of rows
+NUM_ROWS_TRAINNING=X.shape[0]
+# this number has no meaning except for being divisable by 2
+N_MULT_FACTOR=8 # min should be 4
+# Size of first linear layer
+N_HIDDEN=N_FEATURES * N_MULT_FACTOR
+# CNN kernel size
+N_CNN_KERNEL=3
+MAX_POOL_KERNEL=4
+
+    
+class Net2(nn.Module):    
+    def __init__(self, n_feature, n_hidden, n_output, n_cnn_kernel, n_mult_factor=N_MULT_FACTOR):
+        super(Net2, self).__init__()
+        self.n_feature=n_feature
+        self.n_hidden=n_hidden
+        self.n_output= n_output 
+        self.n_cnn_kernel=n_cnn_kernel
+        self.n_mult_factor=n_mult_factor
+        self.n_l2_hidden=self.n_hidden * (self.n_mult_factor - self.n_cnn_kernel + 3)
+#         self.n_out_hidden=int (self.n_l2_hidden/2)
+                        
+        self.l1 = nn.Sequential(
+            torch.nn.Linear(self.n_feature, self.n_hidden),
+            torch.nn.Dropout(p=1 -.85),            
+            torch.nn.LeakyReLU (0.1),            
+            torch.nn.BatchNorm1d(self.n_hidden, eps=1e-05, momentum=0.1, affine=True)            
+        )                
+        self.c1= nn.Sequential(            
+            torch.nn.Conv1d(self.n_feature, self.n_hidden, 
+                            kernel_size=(self.n_cnn_kernel,), stride=(1,), padding=(1,)),
+            torch.nn.Dropout(p=1 -.75),            
+            torch.nn.LeakyReLU (0.1),
+            torch.nn.BatchNorm1d(self.n_hidden, eps=1e-05, momentum=0.1, affine=True)        
+        )                        
+        self.out = nn.Sequential(
+            torch.nn.Linear(self.n_l2_hidden,
+                            self.n_output),  
+        )                
+        self.sig=nn.Sigmoid()
+
+        
+    def forward(self, x):
+        
+        varSize=x.data.shape[0] # must be calculated here in forward() since its is a dynamic size        
+        x=self.l1(x)                
+        
+        # for CNN        
+        x = x.view(varSize,self.n_feature,self.n_mult_factor)
+        
+        x=self.c1(x)
+        
+        # for Linear layer
+        x = x.view(varSize, self.n_hidden * (self.n_mult_factor -self.n_cnn_kernel + 3))
+#         x=self.l2(x)                    
+        x=self.out(x) 
+        x=self.sig(x)
+        return x
+    
+net = Net2(n_feature=N_FEATURES, n_hidden=N_HIDDEN, n_output=13, n_cnn_kernel=N_CNN_KERNEL)   # define the network    
+
+
+optimizer = torch.optim.Adam(net.parameters(), lr=0.005,weight_decay=5e-4) #  L2 regularization
+loss_func=torch.nn.BCELoss() # Binary cross entropy: http://pytorch.org/docs/nn.html#bceloss
+
+epochs=100
+all_losses = []
+
+X_tensor_train= XnumpyToTensor(X)
+Y_tensor_train= YnumpyToTensor(y)
+
+print(type(X_tensor_train.data), type(Y_tensor_train.data)) # should be 'torch.cuda.FloatTensor'
+
+# From here onwards, we must only use PyTorch Tensors
+for step in range(epochs):
+    out = net(X_tensor_train)                 # input x and predict based on x
+    cost = loss_func(out, Y_tensor_train)     # must be (1. nn output, 2. target), the target label is NOT one-hotted
+
+    optimizer.zero_grad()   # clear gradients for next train
+    cost.backward()         # backpropagation, compute gradients
+    optimizer.step()        # apply gradients
+                           
+    if step % 10 == 0:        
+        loss = cost.data
+        all_losses.append(loss)
+        print(step, cost.data.cpu().numpy())        
+        prediction = (net(X_tensor_train).data).float() # probabilities             
+        pred_y = prediction.cpu().numpy().squeeze()
+        target_y = Y_tensor_train.cpu().data.numpy()                        
+        tu = ((pred_y == target_y).mean(),log_loss(target_y, pred_y),roc_auc_score(target_y,pred_y ))
+        print ('ACC={}, LOG_LOSS={}, ROC_AUC={} '.format(*tu))        
+                
+import matplotlib.pyplot as plt
+plt.plot(all_losses)
+plt.show()
+
+false_positive_rate, true_positive_rate, thresholds = roc_curve(target_y,pred_y)
+roc_auc = auc(false_positive_rate, true_positive_rate)
+
+plt.title('LOG_LOSS=' + str(log_loss(target_y, pred_y)))
+plt.plot(false_positive_rate, true_positive_rate, 'b', label='AUC = %0.6f' % roc_auc)
+plt.legend(loc='lower right')
+plt.plot([0, 1], [0, 1], 'r--')
+plt.xlim([-0.1, 1.2])
+plt.ylim([-0.1, 1.2])
+plt.ylabel('True Positive Rate')
+plt.xlabel('False Positive Rate')
+plt.show()
