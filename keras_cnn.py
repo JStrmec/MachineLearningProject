bfdd3f01e95f103763c6f2c5092b666341de5a22
Chase Conaway <chase.conaway@wsu.edu>
2022-04-22 11:13:59 -0700

    My cnn is now good

--- keras_cnn.py
+++ keras_cnn.py
@@ -1,59 +1,92 @@
-from tensorflow import keras
-import keras.layers as layers
-import matplotlib.pyplot as plt
+import tensorflow as tf
 
-LAYERS = 52 # arbitrary number for now
+from unicodedata import name
+import tensorflow as tf
+import keras.layers as layers
+from keras.layers import BatchNormalization, Dense, Reshape, Flatten, Conv1D, Concatenate, Dropout
+import tensorflow_addons as tfa
+from tensorflow import keras
+import matplotlib.pyplot as plt
+from keras import datasets, layers, models
+import tensorflow_addons as tfa
+from tensorflow import keras
+import matplotlib.pyplot as plt
+import data_processing as d
+from sklearn.model_selection import train_test_split
+import numpy as np
+
+KERNEL_SIZE = 3
+FILTERS = 128
+DROPOUT_RATE = 0.25
+
 EPOCHS = 1000 # arbitrary
 
-# TODO: define all the testing data
+X, y = d.get_encoded_data()
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.33, random_state=1)
 
 def getModel():
-    # all three layers are arbitrarily chosen, don't know input shape
     return keras.Sequential(
         [
-            layers.Conv1D(596, 4, activation='relu', input_shape=(None,13)),
-            layers.Dense(LAYERS / 2, activation='relu', name='layer2'),
-            layers.Dropout(.3),
-            layers.Conv1D(298, 2, activation='relu'),
-            layers.Dense(LAYERS / 4, activation='relu', name='layer3'),
-            layers.Dropout(.3),
-            layers.Dense(13, name='output_layer'),
+            layers.Conv1D(FILTERS, KERNEL_SIZE, padding='same', activation='relu', name="layer1", input_shape=X_train.shape[1:]),
+            layers.LeakyReLU(),
+            layers.Dropout(DROPOUT_RATE),
+            layers.Conv1D(FILTERS / 2, KERNEL_SIZE, padding='same', activation='relu', name="layer2"),
+            layers.LeakyReLU(),
+            layers.Dropout(DROPOUT_RATE),
+            layers.Conv1D(FILTERS / 4, KERNEL_SIZE, padding='same', activation='relu', name="layer3"),
+            layers.LeakyReLU(),
+            layers.Dropout(DROPOUT_RATE),
+            layers.Dense(13, activation='softmax', name='output_layer')
         ])
 
 def plot(history, epochs, metric, type):
-    loss_train = history.history[metric]
-    epochs = range(1,epochs)
+    loss_train = history[metric]
+    epochs = range(1,epochs + 1)
     plt.plot(epochs, loss_train, 'g', label=metric)
-    plt.title(type, metric)
+    plt.title(metric)
     plt.xlabel('Epochs')
     plt.ylabel(metric)
     plt.legend()
     plt.show()
 
 if __name__ == '__main__':
+    # print(X_train[0], y_train)
+    X_train = X_train.reshape(1, X_train.shape[0], X_train.shape[1])
+    y_train = y_train.reshape(1, y_train.shape[0], y_train.shape[1])
+    X_test = X_test.reshape(1, X_test.shape[0], X_test.shape[1])
+    y_test = y_test.reshape(1, y_test.shape[0], y_test.shape[1])
+    
+    X_train = X_train.astype(np.float32)
+    y_train = y_train.astype(np.float32)
+    
+    
+    # for i in train_dataset:
+    #     print(i)
     model = getModel()
-    
-    optimizer = keras.optimizers.Adam(learning_rate=0.001)
-    
-    model.compile(optimizer, 'mse', metrics=[keras.metrics.Accuracy(), 
-                                                keras.metrics.MeanSquaredError(),
-                                                keras.metrics.Precision(), 
-                                                keras.metrics.Recall(),
-                                                keras.metrics.RootMeanSquaredError()])
-    
+    optimizer = keras.optimizers.Nadam(learning_rate=1e-3, decay=5e-4)
+   
+    model.compile(optimizer, 'mse', metrics=[keras.metrics.Accuracy(),
+                                                # keras.metrics.Precision(), 
+                                                # keras.metrics.Recall()
+                                                ])
+
+    # model.build()
+    # model.summary()
     history = model.fit(
         X_train,
         y_train,
-        batch_size=64,
+        batch_size=32,
         epochs=EPOCHS
     )
-    
-    for metric in history:
-        plot(history, EPOCHS, metric, "Training")
-    
+   
+    for metric in history.history:
+        plot(history.history, EPOCHS, metric, "Training")
+   
     print("Evaluate on test data")
-    results = model.evaluate(X_test, y_test, batch_size=128)
+    results = model.evaluate(X_train, y_train)
     print("test loss, test acc:", results)
-    
-    for metric in results:
-        plot(results, EPOCHS, metric, "Testing")
+   
+    # for metric in results:
+    #     plt.plot(metric)
+    # plt.show()
