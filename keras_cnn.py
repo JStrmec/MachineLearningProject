4d11653bf21eb8c765c06395ba6cce7083a4556e
Chase Conaway <chase.conaway@wsu.edu>
2022-04-22 13:59:28 -0700

    Working on getting accuarcy up

--- keras_cnn.py
+++ keras_cnn.py
@@ -17,26 +17,19 @@ import numpy as np
 # X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.33, random_state=1)
 
 class keras_CNN():
-    def __init__(self, X_train, X_test, y_train, y_test, kernel_size, epochs):
+    def __init__(self, kernel_size, epochs):
         self.EPOCHS = epochs
         self.KERNEL_SIZE = kernel_size
-        self.X_train = X_train
-        self.X_test = X_test
-        self.y_train = y_train
-        self.y_test = y_test
-        self.FILTER = 256
-        self.DROPOUT_RATE = 0.25
+        self.FILTERS = 64
+        self.DROPOUT_RATE = 0.3
            
-    def getModel(self):
+    def getModel(self, X_train):
         return keras.Sequential(
             [
-                layers.Conv1D(self.FILTERS, self.KERNEL_SIZE, padding='same', activation='relu', name="layer1", input_shape=self.X_train.shape[1:]),
+                layers.Conv1D(self.FILTERS, self.KERNEL_SIZE, padding='same', activation='relu', name="layer1"),
                 layers.LeakyReLU(),
                 layers.Dropout(self.DROPOUT_RATE),
                 layers.Conv1D(self.FILTERS / 2, self.KERNEL_SIZE, padding='same', activation='relu', name="layer2"),
-                layers.LeakyReLU(),
-                layers.Dropout(self.DROPOUT_RATE),
-                layers.Conv1D(self.FILTERS / 4, self.KERNEL_SIZE, padding='same', activation='relu', name="layer3"),
                 layers.LeakyReLU(),
                 layers.Dropout(self.DROPOUT_RATE),
                 layers.Dense(13, activation='softmax', name='output_layer')
@@ -51,19 +44,20 @@ class keras_CNN():
         plt.ylabel(metric)
         plt.legend()
         plt.show()
+        
+    def reshape_and_chng_type(self, X_train, X_test, y_train, y_test):
+        new_X_train = X_train.reshape(1, X_train.shape[0], X_train.shape[1])
+        new_y_train = y_train.reshape(1, y_train.shape[0], y_train.shape[1])
+        new_X_test = X_test.reshape(1, X_test.shape[0], X_test.shape[1])
+        new_y_test = y_test.reshape(1, y_test.shape[0], y_test.shape[1])
+        
+        new_X_train = new_X_train.astype(np.float32)
+        new_y_train = new_y_train.astype(np.float32)
+        new_X_test = new_X_test.astype(np.float32)
+        new_y_test = new_y_test.astype(np.float32)
+        return new_X_train, new_X_test, new_y_train, new_y_test
 
-    def main(self):
-        # print(X_train[0], y_train)
-        self.X_train = self.X_train.reshape(1, self.X_train.shape[0], self.X_train.shape[1])
-        self.y_train = self.y_train.reshape(1, self.y_train.shape[0], self.y_train.shape[1])
-        self.X_test = self.X_test.reshape(1, self.X_test.shape[0], self.X_test.shape[1])
-        self.y_test = self.y_test.reshape(1, self.y_test.shape[0], self.y_test.shape[1])
-        
-        self.X_train = self.X_train.astype(np.float32)
-        self.y_train = self.y_train.astype(np.float32)
-        self.X_test = self.X_test.astype(np.float32)
-        self.y_test = self.y_test.astype(np.float32)
-        
+    def main(self):        
         model = self.getModel()
         optimizer = keras.optimizers.Nadam(learning_rate=1e-3, decay=5e-4)
     
@@ -78,7 +72,6 @@ class keras_CNN():
             self.X_train,
             self.y_train,
             batch_size=32,
-            steps_per_epoch=20,
             epochs=self.EPOCHS
         )
     
@@ -88,7 +81,3 @@ class keras_CNN():
         print("Evaluate on test data")
         results = model.predict(self.X_test, batch_size=32)
         print("test loss, test acc:", accuracy_score(results, self.y_test))
-    
-        # for metric in results:
-        #     plt.plot(metric)
-        # plt.show()
