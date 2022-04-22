e6d155cfc9e4a8732eb74c0754dad9c90fbc62cd
Chase Conaway <chase.conaway@wsu.edu>
2022-04-22 11:38:28 -0700

    Added gitignore and made Keras Cnn a class

--- keras_cnn.py
+++ keras_cnn.py
@@ -1,14 +1,4 @@
-import tensorflow as tf
-
-from unicodedata import name
-import tensorflow as tf
 import keras.layers as layers
-from keras.layers import BatchNormalization, Dense, Reshape, Flatten, Conv1D, Concatenate, Dropout
-import tensorflow_addons as tfa
-from tensorflow import keras
-import matplotlib.pyplot as plt
-from keras import datasets, layers, models
-import tensorflow_addons as tfa
 from tensorflow import keras
 import matplotlib.pyplot as plt
 import data_processing as d
@@ -16,77 +6,85 @@ from sklearn.model_selection import train_test_split
 import numpy as np
 
 KERNEL_SIZE = 3
-FILTERS = 128
+FILTERS = 256
 DROPOUT_RATE = 0.25
 
-EPOCHS = 1000 # arbitrary
+EPOCHS = 100 # arbitrary
 
 X, y = d.get_encoded_data()
 
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.33, random_state=1)
 
-def getModel():
-    return keras.Sequential(
-        [
-            layers.Conv1D(FILTERS, KERNEL_SIZE, padding='same', activation='relu', name="layer1", input_shape=X_train.shape[1:]),
-            layers.LeakyReLU(),
-            layers.Dropout(DROPOUT_RATE),
-            layers.Conv1D(FILTERS / 2, KERNEL_SIZE, padding='same', activation='relu', name="layer2"),
-            layers.LeakyReLU(),
-            layers.Dropout(DROPOUT_RATE),
-            layers.Conv1D(FILTERS / 4, KERNEL_SIZE, padding='same', activation='relu', name="layer3"),
-            layers.LeakyReLU(),
-            layers.Dropout(DROPOUT_RATE),
-            layers.Dense(13, activation='softmax', name='output_layer')
-        ])
+class keras_CNN():
+    def __init__(self, X_train, X_test, y_train, y_test, kernel_size, epochs):
+        self.EPOCHS = epochs
+        self.KERNEL_SIZE = kernel_size
+        self.X_train = X_train
+        self.X_test = X_test
+        self.y_train = y_train
+        self.y_test = y_test
+        
+    def getModel(self):
+        return keras.Sequential(
+            [
+                layers.Conv1D(FILTERS, KERNEL_SIZE, padding='same', activation='relu', name="layer1", input_shape=X_train.shape[1:]),
+                layers.LeakyReLU(),
+                layers.Dropout(DROPOUT_RATE),
+                layers.Conv1D(FILTERS / 2, KERNEL_SIZE, padding='same', activation='relu', name="layer2"),
+                layers.LeakyReLU(),
+                layers.Dropout(DROPOUT_RATE),
+                layers.Conv1D(FILTERS / 4, KERNEL_SIZE, padding='same', activation='relu', name="layer3"),
+                layers.LeakyReLU(),
+                layers.Dropout(DROPOUT_RATE),
+                layers.Dense(13, activation='softmax', name='output_layer')
+            ])
 
-def plot(history, epochs, metric, type):
-    loss_train = history[metric]
-    epochs = range(1,epochs + 1)
-    plt.plot(epochs, loss_train, 'g', label=metric)
-    plt.title(metric)
-    plt.xlabel('Epochs')
-    plt.ylabel(metric)
-    plt.legend()
-    plt.show()
+    def plot(self, history, epochs, metric):
+        loss_train = history[metric]
+        epochs = range(1,epochs + 1)
+        plt.plot(epochs, loss_train, 'g', label=metric)
+        plt.title(metric)
+        plt.xlabel('Epochs')
+        plt.ylabel(metric)
+        plt.legend()
+        plt.show()
 
-if __name__ == '__main__':
-    # print(X_train[0], y_train)
-    X_train = X_train.reshape(1, X_train.shape[0], X_train.shape[1])
-    y_train = y_train.reshape(1, y_train.shape[0], y_train.shape[1])
-    X_test = X_test.reshape(1, X_test.shape[0], X_test.shape[1])
-    y_test = y_test.reshape(1, y_test.shape[0], y_test.shape[1])
+    def main(self):
+        # print(X_train[0], y_train)
+        self.X_train = self.X_train.reshape(1, self.X_train.shape[0], self.X_train.shape[1])
+        self.y_train = self.y_train.reshape(1, self.y_train.shape[0], self.y_train.shape[1])
+        self.X_test = self.X_test.reshape(1, self.X_test.shape[0], self.X_test.shape[1])
+        self.y_test = self.y_test.reshape(1, self.y_test.shape[0], self.y_test.shape[1])
+        
+        self.X_train = self.X_train.astype(np.float32)
+        self.y_train = self.y_train.astype(np.float32)
+        self.X_test = self.X_test.astype(np.float32)
+        self.y_test = self.y_test.astype(np.float32)
+        
+        model = self.getModel()
+        optimizer = keras.optimizers.Nadam(learning_rate=1e-3, decay=5e-4)
     
-    X_train = X_train.astype(np.float32)
-    y_train = y_train.astype(np.float32)
-    
-    
-    # for i in train_dataset:
-    #     print(i)
-    model = getModel()
-    optimizer = keras.optimizers.Nadam(learning_rate=1e-3, decay=5e-4)
-   
-    model.compile(optimizer, 'mse', metrics=[keras.metrics.Accuracy(),
-                                                # keras.metrics.Precision(), 
-                                                # keras.metrics.Recall()
-                                                ])
+        model.compile(optimizer, 'mse', metrics=[keras.metrics.Accuracy(),
+                                                    # keras.metrics.Precision(), 
+                                                    # keras.metrics.Recall()
+                                                    ])
 
-    # model.build()
-    # model.summary()
-    history = model.fit(
-        X_train,
-        y_train,
-        batch_size=32,
-        epochs=EPOCHS
-    )
-   
-    for metric in history.history:
-        plot(history.history, EPOCHS, metric, "Training")
-   
-    print("Evaluate on test data")
-    results = model.evaluate(X_train, y_train)
-    print("test loss, test acc:", results)
-   
-    # for metric in results:
-    #     plt.plot(metric)
-    # plt.show()
+        # model.build()
+        # model.summary()
+        history = model.fit(
+            X_train,
+            y_train,
+            batch_size=32,
+            epochs=EPOCHS
+        )
+    
+        for metric in history.history:
+            self.plot(history.history, EPOCHS, metric, "Training")
+    
+        print("Evaluate on test data")
+        results = model.evaluate(X_train, y_train)
+        print("test loss, test acc:", results)
+    
+        # for metric in results:
+        #     plt.plot(metric)
+        # plt.show()
